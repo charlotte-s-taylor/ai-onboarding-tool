db61e4cf4019285de7d1070e918b7fa34b62adb1
charlotte-s-taylor <46196571+charlotte-s-taylor@users.noreply.github.com>
2025-08-01 01:08:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import OnboardingModal from './components/OnboardingModal';
-import DemoPage from './pages/DemoPage';
 
 function App() {
-  const [formData, setFormData] = useState(null);
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<OnboardingModal onComplete={setFormData} />} />
-        <Route path="/demo" element={<DemoPage formData={formData} />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <OnboardingModal />
+    </div>
   );
 }
 
