3ddbb043452b2ee83393ea34ffb1f7ebb98d314a
charlotte-s-taylor <46196571+charlotte-s-taylor@users.noreply.github.com>
2025-08-01 01:03:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OnboardingModal from './components/OnboardingModal';
 import DemoPage from './pages/DemoPage';
 
