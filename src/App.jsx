b6b87a43e6252316fb3fc013422f163dcade7b1e
charlotte-s-taylor <46196571+charlotte-s-taylor@users.noreply.github.com>
2025-08-02 15:09:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
-import React, { useState } from "react";
-import OnboardingModal from "./components/OnboardingModal";
-import ExperimentModal from "./components/ExperimentModal";
-import ExperimentCard from "./components/ExperimentCard";
-import Toast from "./components/temp-toast";
-import "./main.css";
+import React from 'react';
 
 function App() {
-  const [showOnboarding, setShowOnboarding] = useState(true);
-  const [showExperimentModal, setShowExperimentModal] = useState(false);
-  const [showToast, setShowToast] = useState(false);
-
-  const handleFinishOnboarding = () => {
-    setShowOnboarding(false);
-  };
-
-  const handleOpenExperimentModal = () => {
-    setShowExperimentModal(true);
-  };
-
-  const handleCloseExperimentModal = () => {
-    setShowExperimentModal(false);
-  };
-
-  const handleCreateExperiment = () => {
-    setShowExperimentModal(false);
-    setShowToast(true);
-    setTimeout(() => setShowToast(false), 3000);
-  };
-
   return (
-  <div>
-    <h1>Hello world from App.jsx</h1>
-  </div>
-);
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
